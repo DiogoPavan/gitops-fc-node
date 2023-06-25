628354c5ec4d40b3b62dc7ddd3d2bee1020b6fa2
Diogo Reis Pavan <diogo.pavan@somosphi.com>
2023-06-25 16:00:22 -0300

    adjust text

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-type', 'text/plain');
-  res.end('Hello world');
+  res.end('Hello Argo');
 });
 
 server.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
