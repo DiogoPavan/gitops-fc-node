2036164bd1f5fdd98c86630f60a495bcc512b7e3
Diogo Reis Pavan <diogo.pavan@somosphi.com>
2023-06-25 11:02:21 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  res.end('Hello world');
+});
+
+server.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
